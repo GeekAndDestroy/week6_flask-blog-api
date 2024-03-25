19d3d9afa4541a33fa62048bf50643eca0a7cad8
Jeff Chebul <jmchebul@jmchebul.com>
2024-03-25 14:42:22 -0500

    Add Get All POsts and Get Single POst byID routes

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
 from app import app
+from fake_data.posts import post_data
 
 # Define a route
 @app.route("/")
@@ -7,16 +8,26 @@ def index():
     age = 55
     return 'Hello ' + first_name + ' who is ' + str(age) + ' years old'
 
-@app.route('/test')
-def test():
-    my_dicts = []
-    for i in range(5):
-        a_dict = {
-            'id': i+1,
-            'key': 'value',
-            'name': 'Brian'
-        }
-        my_dicts.append(a_dict)
-        
-    return my_dicts
 
+# Post endpoints
+
+# get all posts
+@app.route('/posts')
+def get_posts():
+    # get the posts from storage (fake data -> tomorrow will be db)
+    posts = post_data
+    return posts
+
+# Get a single post by ID
+@app.route('/posts/<int:post_id>')
+def get_post(post_id):
+    # get the posts from storage
+    posts = post_data
+    # for each dictionary in the list of post dictionaries
+    for post in posts:
+        #if the key of 'id' matches the post_id from the url
+        if post['id'] == post_id:
+            #return that post dictionary
+            return post
+    # If we loop throuigh all of thge posts without reeturning, the post with that ID does not exist
+    return {'error': f"Post with an ID of {post_id} does not exist"}, 404
