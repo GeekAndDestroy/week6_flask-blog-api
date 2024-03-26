ab351951e49f8dfbff1b1232d81c6242ed9a53e3
Jeff Chebul <jmchebul@jmchebul.com>
2024-03-26 16:19:17 -0500

    Added create user, database magic, migrate, password hashbrowns

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,18 @@
 from flask import Flask # Import the Flask class from the flask module
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import Config
 
 # Create an instance of Flask called app that will be the central object
 app = Flask(__name__)
+#set the configuration for the app
+app.config.from_object(Config)
+
+# create an instance of SQLAlchemy called db which will be the central object for our database
+
+db = SQLAlchemy(app)
+# create an instanmce of Migrate with the app and db
+migrate = Migrate(app, db)
 
 # import the routes to the app
-from app import routes
+from app import routes, models
--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,63 @@
+from flask import request, render_template
 from app import app
 from fake_data.posts import post_data
+
+
+# Will set up db later, for now we will store all Users in this users list
+users = []
+
 
 # Define a route
 @app.route("/")
 def index():
-    first_name = 'Brian'
-    age = 55
-    return 'Hello ' + first_name + ' who is ' + str(age) + ' years old'
+    return render_template('index.html')
+
+#User endpoints
+
+#create a  new user
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    #Check to make sure that the request body is JSON
+    if not request.is_json:
+        return {'error': 'Your content-type must be application/json'}, 400
+    # GET the data from the request body
+    data = request.json
+
+    #Validate that the data has all the required fields
+    required_fields = ['firstName', 'lastName', 'username', 'email', 'password']
+    missing_fields = []
+    for field in required_fields:
+        if field not in data:
+            missing_fields.append(field)
+    if missing_fields:
+        return {'error': f"{', '.join(missing_fields)} must be in the request body"}, 400
+    
+    # Pull the individual data from the body
+    first_name = data.get('firstName')
+    last_name = data.get('lastName')
+    username = data.get('username')
+    email = data.get('email')
+    password = data.get('password')
+    
+   # Check to see if any current users already have that username and/or email
+    for user in users:
+        if user['username'] == username or user['email'] == email:
+            return {'error': "A user with that username and/or email already exists"}, 400 
+        
+    # Create a new instance of user with the data from the request
+    new_user = {
+        "id": len(users) + 1,
+        "firstName": first_name,
+        "lastName": last_name,
+        "username": username,
+        "email": email,
+        "password": password
+    }
+    users.append(new_user)
+
+    return new_user, 201
+
 
 
 # Post endpoints
@@ -31,3 +82,44 @@ def get_post(post_id):
             return post
     # If we loop throuigh all of thge posts without reeturning, the post with that ID does not exist
     return {'error': f"Post with an ID of {post_id} does not exist"}, 404
+
+# Create a Post
+@app.route('/posts', methods=['POST'])
+def create_post():
+    # Check to see if the request body is JSON
+    if not request.is_json:
+        return {'error': 'Your content-type must be application/json'}, 400
+    # Get the data from the request body
+    data = request.json
+    # Validate the incoming data
+    required_fields = ['title', 'body']
+    missing_fields = []
+    # For each of the required fields
+    for field in required_fields:
+        # If the field is not in the request body dictionary
+        if field not in data:
+            # Add that field to the list of missing fields
+            missing_fields.append(field)
+    # If there are any missing fields, return 400 status code with the missing fields listed
+    if missing_fields:
+        return {'error': f"{', '.join(missing_fields)} must be in the request body"}, 400
+
+ # Get data values
+    title = data.get('title')
+    body = data.get('body')
+
+    # Create a new post dictionary with data
+    new_post = {
+        'id': len(post_data) + 1,
+        'title': title,
+        'body': body,
+        'userId': 1,
+        'dateCreated': '2024-03-25T15:21:35',
+        'likes': 0
+    }
+
+    # Add the new post to storage (post_data -> will be db tomorrow)
+    post_data.append(new_post)
+    
+    # Return the newly created post dictionary with a 201 Created Status Code
+    return new_post, 201
