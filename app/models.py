ca6c21791db0261edd03f85dac26a8dbe237814b
Jeff Chebul <jmchebul@jmchebul.com>
2024-03-29 11:03:35 -0500

    added gunicorn

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
-from app import db
-from datetime import datetime, timezone
+import secrets
+from . import db
+from datetime import datetime, timezone, timedelta
 from werkzeug.security import generate_password_hash, check_password_hash
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -8,8 +10,12 @@ class User(db.Model):
     last_name = db.Column(db.String, nullable=False)
     email = db.Column(db.String, nullable=False, unique=True)
     username = db.Column(db.String, nullable=False, unique=True)
-    password = db.Column(db.String, nullable=False,)
+    password = db.Column(db.String, nullable=False)
     date_created = db.Column(db.DateTime, nullable=False, default=lambda: datetime.now(timezone.utc))
+    posts = db.relationship('Post', back_populates='author')
+    comments = db.relationship('Comment', back_populates='user')
+    token = db.Column(db.String, index=True, unique=True)
+    token_expiration = db.Column(db.DateTime(timezone=True))
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -18,26 +24,36 @@ class User(db.Model):
     def __repr__(self):
         return f"<User {self.id}|{self.username}>"
 
-    def save(self):
-        db.session.add(self)
-        db.session.commit()
-
     def set_password(self, plaintext_password):
         self.password = generate_password_hash(plaintext_password)
         self.save()
 
-    def check_pasword(self, plaintext_password):
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def check_password(self, plaintext_password):
         return check_password_hash(self.password, plaintext_password)
-    
+
     def to_dict(self):
         return {
             "id": self.id,
             "firstName": self.first_name,
             "lastName": self.last_name,
             "username": self.username,
+            "email": self.email,
             "dateCreated": self.date_created
         }
     
+    def get_token(self):
+        now = datetime.now(timezone.utc)
+        if self.token and self.token_expiration > now + timedelta(minutes=1):
+            return {"token": self.token, "tokenExpiration": self.token_expiration}
+        self.token = secrets.token_hex(16)
+        self.token_expiration = now + timedelta(hours=1)
+        self.save()
+        return {"token": self.token, "tokenExpiration": self.token_expiration}
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -46,6 +62,8 @@ class Post(db.Model):
     date_created = db.Column(db.DateTime, nullable=False, default=lambda: datetime.now(timezone.utc))
     # In SQL - user_id INTEGER NOT NULL, FOREIGN KEY(user_id) REFERENCES user(id)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    author = db.relationship('User', back_populates='posts')
+    comments = db.relationship('Comment', back_populates="post")
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -58,11 +76,60 @@ class Post(db.Model):
         db.session.add(self)
         db.session.commit()
 
+    def delete(self):
+        db.session.delete(self) # deleting THIS object from the database
+        db.session.commit() # commiting our changes
+
     def to_dict(self):
         return {
             "id": self.id,
             "title": self.title,
             "body": self.body,
             "dateCreated": self.date_created,
-            "user_id": self.user_id
+            "author": self.author.to_dict(),
+            'comments': [comment.to_dict() for comment in self.comments]
+        }
+    
+    def update(self, **kwargs):
+        allowed_fields = {'title', 'body'}
+
+        for key, value in kwargs.items():
+            if key in allowed_fields:
+                setattr(self, key, value)
+        self.save()
+
+# Create our Comment class/table
+class Comment(db.Model):
+    # CREATE TABLE
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String, nullable=False)
+    date_created = db.Column(db.DateTime, nullable=False, default=lambda: datetime.now(timezone.utc))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
+    user = db.relationship('Comment', back_populates='comments')
+    post = db.relationship('Comment', back_populates='comments')
+
+    # INSERT INTO
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.save()
+
+    def __repr__(self):
+        return f"<Comment {self.id}>"
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'body': self.body,
+            'dateCreated': self.date_created,
+            'post_id': self.post_id,
+            'user': self.user.to_dict()
         }
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,7 @@
 from flask import request, render_template
 from app import app, db
-from .models import User, Post
-
-
-# Will set up db later, for now we will store all Users in this users list
-users = []
+from .models import Comment, User, Post
+from .auth import basic_auth, token_auth
 
 
 # Define a route
@@ -50,16 +47,25 @@ def create_user():
     
     return new_user.to_dict(), 201
 
-
+@app.route('/token')
+@basic_auth.login_required
+def get_token():
+    user = basic_auth.current_user()
+    return user.get_token()
 
 # Post endpoints
 
 # get all posts
 @app.route('/posts')
 def get_posts():
-    # get the posts from storage (fake data -> tomorrow will be db)
-    posts = db.session.execute(db.select(Post)).scalars().all()
+    select_stmt = db.select(Post)
+    search = request.args.get('search')
+    if search:
+        select_stmt = select_stmt.where(Post.title.ilike(f"%{search}%"))
+    # get the posts from database
+    posts = db.session.execute(select_stmt).scalars().all()
     return [p.to_dict() for p in posts]
+
 
 # Get a Single Post By ID
 @app.route('/posts/<int:post_id>')
@@ -73,6 +79,7 @@ def get_post(post_id):
 
 # Create a Post
 @app.route('/posts', methods=['POST'])
+@token_auth.login_required
 def create_post():
     # Check to see if the request body is JSON
     if not request.is_json:
@@ -92,22 +99,114 @@ def create_post():
     if missing_fields:
         return {'error': f"{', '.join(missing_fields)} must be in the request body"}, 400
 
- # Get data values
+    # Get data values
     title = data.get('title')
     body = data.get('body')
 
-    # Create a new post dictionary with data
-    new_post = {
-        'id': len(post_data) + 1,
-        'title': title,
-        'body': body,
-        'userId': 1,
-        'dateCreated': '2024-03-25T15:21:35',
-        'likes': 0
-    }
+    current_user = token_auth.current_user()
 
-     # Create a new Post instance with data (and hard-code user_id for time being)
-    new_post = Post(title=title, body=body, user_id=2)
+    # Create a new Post instance with data (and get the id from the token authenticated user)
+    new_post = Post(title=title, body=body, user_id=current_user.id)
     
     # Return the newly created post dictionary with a 201 Created Status Code
     return new_post.to_dict(), 201
+
+
+# Update Post Endpoint
+@app.route('/posts/<int:post_id>', methods=['PUT'])
+@token_auth.login_required
+def edit_post(post_id):
+    # Check to see that they have a JSON body
+    if not request.is_json:
+        return {'error': 'You content-type must be application/json'}, 400
+    # Let's the find post in the db
+    post = db.session.get(Post, post_id)
+    if post is None:
+        return {'error': f"Post with ID #{post_id} does not exist"}, 404
+    # Get the current user based on the token
+    current_user = token_auth.current_user()
+    # Check if the current user is the author of the post
+    if current_user is not post.author:
+        return {'error': "This is not your post. You do not have permission to edit"}, 403
+    
+    # Get the data from the request
+    data = request.json
+    # Pass that data into the post's update method
+    post.update(**data)
+    return post.to_dict()
+
+# Delete a Post
+@app.route('/posts/<int:post_id>', methods=['DELETE'])
+@token_auth.login_required
+def delete_post(post_id):
+    # based on the post_id parameter check to see Post exists
+    post = db.session.get(Post, post_id)
+    
+    if post is None:
+        return {'error': f'Post with {post_id} does not exist. Please try again'}, 404
+    
+    #Make sure user trying to delete post is the user whom created it
+    current_user = token_auth.current_user()
+    if post.author is not current_user:
+        return {'error': 'You do not have permission to delete this post'}, 403
+    
+    #delete the post
+    post.delete()
+    return {'success': f"{post.title} was successfully deleted"}, 200
+
+# COMMENT ENDPOINTS
+
+# Create a comment
+@app.route('/posts/<int:post_id>/comments', methods=['POST'])
+@token_auth.login_required
+def create_comment(post_id):
+    
+    #make sure the request has a body
+    if not request.is_json:
+        return {'error': 'Your content-type must be application/json'}, 400
+    #grab the correct post
+    post = db.session.get(Post, post_id)
+    if post is None:
+        return {'error': f"Post {post_id} does not exist"}, 404
+    
+    data = request.json
+    
+    required_fields = ['body']
+    missing_fields = []
+    for field in required_fields:
+        if field not in data:
+            missing_fields.append(field)
+    
+    if missing_fields:
+        return {'error': f"{", ".join(missing_fields)} must be present in the request body"}, 400
+    
+    body = data.get('body')
+    current_user = token_auth.current_user()
+    new_comment = Comment(body=body,user_id=current_user.id, post_id=post.id)
+    return new_comment.to_dict(), 201
+
+# Delete a comment
+@app.route('/posts/<int:post_id>/comments/<int:comment_id>', methods=['DELETE'])
+@token_auth.login_required
+def delete_comment(post_id, comment_id):
+    
+    #grab our post by id 
+    post = db.session.get(Post, post_id)
+    
+    if post is None:
+        return {'error': f"Post {post_id} does not exist"}, 404
+    
+    comment = db.session.get(Comment, comment_id)
+    
+    if comment is None:
+        return {'error': f"Comment {comment_id} does not exist"}, 404
+    
+    current_user = token_auth.current_user()
+    
+    if comment.user != current_user:
+        return {'error': 'You do not have permission to delete this comment'}, 403
+    
+    comment.delete()
+    return {'success': "Comment has been successfully deleted"}, 200
+
+
